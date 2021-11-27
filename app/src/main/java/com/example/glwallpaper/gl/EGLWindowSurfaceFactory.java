dac09f49a72a7f9a6a95a7be8c7c1333cbb1ff7a
wangkunlin <1040057694@qq.com>
2021-11-27 17:41:27 +0800

    修改代码
    Signed-off-by: wangkunlin <1040057694@qq.com>

--- app/src/main/java/com/example/glwallpaper/gl/EGLWindowSurfaceFactory.java
+++ app/src/main/java/com/example/glwallpaper/gl/EGLWindowSurfaceFactory.java
@@ -1,19 +1,9 @@
 package com.example.glwallpaper.gl;
 
-import javax.microedition.khronos.egl.EGL10;
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.egl.EGLDisplay;
-import javax.microedition.khronos.egl.EGLSurface;
+import android.opengl.GLSurfaceView;
 
 /**
  * On 2021-11-20
  */
-public interface EGLWindowSurfaceFactory {
-    /**
-     * @return null if the surface cannot be constructed.
-     */
-    EGLSurface createWindowSurface(EGL10 egl, EGLDisplay display, EGLConfig config,
-                                   Object nativeWindow);
-
-    void destroySurface(EGL10 egl, EGLDisplay display, EGLSurface surface);
+public interface EGLWindowSurfaceFactory extends GLSurfaceView.EGLWindowSurfaceFactory {
 }
